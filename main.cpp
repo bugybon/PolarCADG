efc92cada95e8b9aec79fa509e8f11350790a91c
Bemba <bugybon1@gmail.com>
2024-02-09 01:14:27 +0200

    Right click dot movement added

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 // #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <nuklear.h>
+#include <nuklear_glfw_gl2.h>
 #include "linmath.h"
 
-#include <algorithm>
-
+#include <cmath>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -16,11 +16,14 @@
 
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 600
+#define POINT_RADIUS 6
 
 typedef std::pair<GLdouble,GLdouble> coords;
 typedef std::vector<coords> points;
 
 points dots;
+bool movePoint;
+std::size_t gottenPoint;
 
 coords deCasteljau(std::size_t r, std::size_t i, double t)
 {
@@ -93,20 +96,47 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         // std::cout << " & x: " << dots.back().first + SCREEN_WIDTH/2 << " y: " << dots.back().second + SCREEN_HEIGHT/2 << std::endl;
         std::cout << " lenght: " << dots.size() << std::endl;
     }
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        GLdouble xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        xpos = xpos - SCREEN_WIDTH/2;
+        ypos = ypos - SCREEN_HEIGHT/2;
+        for(std::size_t i = 0; i < dots.size(); ++i)
+        {
+            if(std::pow(dots[i].first - xpos, 2) + std::pow(dots[i].second - ypos, 2) <= POINT_RADIUS * POINT_RADIUS)
+            {   
+                movePoint = true;
+                gottenPoint = i;
+                std::cout << "gotten" << std::endl;
+            }
+        }
+    }
+
+    if(button == GLFW_MOUSE_BUTTON_RIGHT && action != GLFW_PRESS){
+        movePoint = false;
+        gottenPoint = -1;
+    }
 }
 
 int main(void)
 {   
     glfwInit();
-    glfwSetErrorCallback(error_callback);
     glfwWindowHint(GLFW_SAMPLES, 4);
 
+    /* Platform */
+    static GLFWwindow *window;
+    int width = 0, height = 0;
+    struct nk_context *ctx;
+    struct nk_colorf bg;
 
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    /* GLFW */
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stdout, "[GFLW] failed to init!\n");
+        exit(1);
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Polar", NULL, NULL);
@@ -126,27 +156,17 @@ int main(void)
 
     points bezierPoints, polarPoints, polarLines;
     int powerPolar = 2;
+    gottenPoint = -1;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        //   if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-        //                  NK_WINDOW_BORDER |
-        //                  NK_WINDOW_MOVABLE |
-        //                  NK_WINDOW_SCALABLE |
-        //                  NK_WINDOW_MINIMIZABLE |
-        //                  NK_WINDOW_TITLE)) {
-        //     // ..
-        //     nk_layout_row_static(ctx, 30, 80, 1);
-        //     if (nk_button_label(ctx, "button"))
-        //         fprintf(stdout, "button pressed\n");
-        //     }
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
 
         glEnable( GL_POINT_SMOOTH ); // make the point circular
-        glPointSize( 10 ); // must be added before glDrawArrays is called
+        glPointSize( POINT_RADIUS * 2 - 2); // must be added before glDrawArrays is called
         glColor3f(1.0f, 1.0f, 1.0f);
         glBegin( GL_POINTS);
         for(auto dot : dots)
@@ -156,7 +176,7 @@ int main(void)
         glEnd();
         glDisable( GL_POINT_SMOOTH ); // stop the smoothing to make the points circular
 
-        glLineWidth(2);
+        glLineWidth(3);
         glColor3f(0.0f, 0.0f, 0.0f);
         glBegin( GL_LINES);
         for(std::size_t i = 1; i < dots.size(); ++i)
@@ -165,6 +185,15 @@ int main(void)
             glVertex3d(dots[i].first / (SCREEN_WIDTH / 2), - dots[i].second / (SCREEN_HEIGHT / 2), 0);
         }
         glEnd();
+
+        if(gottenPoint != -1){
+            GLdouble xpos, ypos;
+            glfwGetCursorPos(window, &xpos, &ypos);
+            xpos = xpos - SCREEN_WIDTH/2;
+            ypos = ypos - SCREEN_HEIGHT/2;
+            dots[gottenPoint].first = xpos;
+            dots[gottenPoint].second = ypos;
+        }
 
         if(dots.size() > 2)
         {
